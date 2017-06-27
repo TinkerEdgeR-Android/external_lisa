a1833cbacbb9b72ed2fdb4144031d319d1437972
Joel Fernandes <joelaf@google.com>
2017-06-27 13:25:40 -0700

    workload: run custom hooks at different points of workload
    Change-Id: I8d0934a9042ffa4db864433b0b73a6bd91e94e1e
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- libs/utils/android/workload.py
+++ libs/utils/android/workload.py
@@ -47,6 +47,9 @@ class Workload(object):
         self.trace_file = None
         self.nrg_report = None
 
+        # Hooks to run at different points of workload execution
+        self.hooks = {}
+
     def _adb(self, cmd):
         return 'adb -s {} {}'.format(self._target.adb_name, cmd)
 
@@ -92,6 +95,12 @@ class Workload(object):
 
         return cls._availables[name.lower()](test_env)
 
+    def add_hook(self, hook, hook_fn):
+        allowed = ['post_collect_start']
+        if hook not in allowed:
+            return
+        self.hooks[hook] = hook_fn
+
     def run(self, out_dir, collect='',
             **kwargs):
         raise RuntimeError('Not implemented')
@@ -126,6 +135,11 @@ class Workload(object):
         if 'energy' in self.collect and self._te.emeter:
             self._te.emeter.reset()
             self._log.info('Energy meter STARTED')
+        # Run post collect hooks passed added by the user of wload object
+        if 'post_collect_start' in self.hooks:
+            hookfn = self.hooks['post_collect_start']
+            self._log.info("Running post collect startup hook {}".format(hookfn.__name__))
+            hookfn()
 
     def tracingStop(self):
         # Collect energy meter results
